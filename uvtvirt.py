d14cea66ce39a0b1dc00b8bece4757d94b34b856
Michael Reed <Michael.Reed@canonical.com>
2020-10-13 18:57:53 -0500

    Added comments

--- uvtvirt.py
+++ uvtvirt.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python3
 """
-  uvtvirt.py
+    Test kvm using uvtool
+    uvtvirt.py
+    usage: uvtvirt.py [-h] [-i IMAGE] [--debug] [-l LOG_FILE]
+
+    Run uvt-kvm with defaults
+    uvtvirt.py --debug
+
+    Specify a location for uvt-simplestreams-libvirt to retrive an image
+    uvtvirt.py -i http://cloud-images.ubuntu.com/daily/server/daily/
+
+    Specify a cloud image for uvt-kvm create to create a vm
+    uvtvirt.py -i file:///home/ubuntu/focal-server-cloudimg-amd64.img
 """
 
 from argparse import ArgumentParser
